61b4babd92d5b669e880d870b6e008296abbb9f7
TsukiSenn <154390155+TsukiSenn@users.noreply.github.com>
2024-02-11 17:47:50 +0800

    Update helloGit.js

--- helloGit.js
+++ helloGit.js
@@ -1,1 +1,15 @@
 console.log('Hello Git!');
+function Person(name,age,gender,profession,hobby) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.profession = profession;
+  this.hobby = hobby;
+  this.say = function() {
+    console.log(`Name: ${this.name},
+                 Age: ${this.age},
+                 Gender: ${this.gender},
+                 Profession: ${this.profession},
+                 Hobby: ${this.hobby}.`);
+  }
+}
